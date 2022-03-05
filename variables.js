cd49969104c3574766eb84ced9c855442bf9f467
IliaKuzichev <72195701+IliaKuzichev@users.noreply.github.com>
2022-03-05 12:31:58 +0700

    Add files via upload
    variables task

---
+++ variables.js
@@ -0,0 +1,18 @@
+// First task
+/*
+"use strict";
+let admin;
+let name = 'John';
+admin = name;
+alert( admin );   
+*/
+
+// Second task
+let nameOurPlanet;
+let thisMometWebsiteUser;
+
+//Third task
+
+const BIRTHDAY = '18.04.1982';
+
+const age = somecode(BIRTHDAY);
