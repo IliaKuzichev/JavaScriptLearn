0e729ab1bd1f7982f78aab569ebba78bbe0f6e4c
IliaKuzichev <72195701+IliaKuzichev@users.noreply.github.com>
2022-06-23 18:07:44 +0700

    second task

--- string_tasks.js
+++ string_tasks.js
@@ -12,3 +12,17 @@ function ucFirst(str) {
         return str;
     };
 };
+
+/*
+Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+
+Функция должна быть нечувствительна к регистру:
+
+checkSpam('buy ViAgRA now') == true
+checkSpam('free xxxxx') == true
+checkSpam("innocent rabbit") == false
+*/
+
+function checkSpam(str) {
+    return (str.toLowerCase().includes(`xxx`) || str.toLowerCase().includes(`viagra`));
+};
