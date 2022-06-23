b74e6c1c78980a1466fd55d8d2923731a195eda0
IliaKuzichev <72195701+IliaKuzichev@users.noreply.github.com>
2022-06-23 18:17:25 +0700

    third task

--- string_tasks.js
+++ string_tasks.js
@@ -26,3 +26,24 @@ checkSpam("innocent rabbit") == false
 function checkSpam(str) {
     return (str.toLowerCase().includes(`xxx`) || str.toLowerCase().includes(`viagra`));
 };
+
+/*
+Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, если она превосходит maxlength, заменяет конец str на "…", так, чтобы её длина стала равна maxlength.
+
+Результатом функции должна быть та же строка, если усечение не требуется, либо, если необходимо, усечённая строка.
+
+Например:
+
+truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) = "Вот, что мне хотело…"
+
+truncate("Всем привет!", 20) = "Всем привет!"
+*/
+
+function truncate(str, maxlength) {
+    if (str.length > maxlength) {
+        str = str.slice(0, maxlength - 1);
+        return str + `…`
+    } else {
+        return str;
+    };
+};
