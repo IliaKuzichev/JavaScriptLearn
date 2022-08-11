14190a09593acdf7a119da45c27bef454d614a48
IliaKuzichev <72195701+IliaKuzichev@users.noreply.github.com>
2022-08-11 11:15:08 +0300

    third task

--- date.js
+++ date.js
@@ -35,3 +35,33 @@ function getWeekDay(date) {
             return `СБ`;
     };
 };
+
+/* 3
+День недели в европейской нумерации
+важность: 5
+В Европейских странах неделя начинается с понедельника (день номер 1), 
+затем идёт вторник (номер 2) и так до воскресенья (номер 7). Напишите функцию getLocalDay(date), 
+которая возвращает «европейский» день недели для даты date.
+
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+alert( getLocalDay(date) );       // вторник, нужно показать 2
+*/
+
+function getLocalDay(date) {
+    switch (date.getDay()) {
+        case 0:
+            return 7;
+        case 1:
+            return 1;
+        case 2:
+            return 2;
+        case 3:
+            return 3;
+        case 4:
+            return 4;
+        case 5:
+            return 5;
+        case 6:
+            return 6;
+    };
+};
