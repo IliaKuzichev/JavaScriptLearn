4ee5bdfbc82e90c6640c40ea7c3ee20e3a3eec70
IliaKuzichev <72195701+IliaKuzichev@users.noreply.github.com>
2022-08-14 13:24:19 +0300

    seventh task

--- date.js
+++ date.js
@@ -119,3 +119,19 @@ function getSecondsToday() {
     let today = new Date();
     return today.getHours() * 3600 + today.getMinutes() * 60 + today.getSeconds;
 };
+
+/* 7
+Сколько секунд осталось до завтра?
+Создайте функцию getSecondsToTomorrow(), возвращающую количество секунд до завтрашней даты.
+
+Например, если сейчас 23:00, то:
+
+getSecondsToTomorrow() == 3600
+P.S. Функция должна работать в любой день, т.е. в ней не должно быть конкретного значения сегодняшней даты.
+*/
+
+function getSecondsToTomorrow() {
+  let today = new Date();
+  let tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+    return Math.round((tomorrow.getTime() - today.getTime()) / 1000);
+};
