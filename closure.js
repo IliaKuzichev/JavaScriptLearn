2a6e90d1e637613ed4b206ddb1ddb89d82f6f809
IliaKuzichev <72195701+IliaKuzichev@users.noreply.github.com>
2022-09-06 14:31:59 +0300

    sixth and seventh task

--- closure.js
+++ closure.js
@@ -124,3 +124,76 @@ function inArray(arr) {
     return arr.includes(x);
   };
 };
+
+/* 6
+Сортировать по полю
+У нас есть массив объектов, который нужно отсортировать:
+
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+Обычный способ был бы таким:
+
+// по имени (Ann, John, Pete)
+users.sort((a, b) => a.name > b.name ? 1 : -1);
+
+// по возрасту (Pete, Ann, John)
+users.sort((a, b) => a.age > b.age ? 1 : -1);
+Можем ли мы сделать его короче, например вот таким?
+
+users.sort(byField('name'));
+users.sort(byField('age'));
+То есть чтобы вместо функции мы просто писали byField(fieldName).
+
+Напишите функцию byField, которая может быть использована для этого.
+*/
+
+function byField(fieldName) {
+  return (a, b) => a[fieldName] > b[fieldName] ? 1 : -1;
+};
+
+/* 7
+Армия функций
+Следующий код создаёт массив из стрелков (shooters).
+
+Каждая функция предназначена выводить их порядковые номера. Но что-то пошло не так…
+
+function makeArmy() {
+  let shooters = [];
+
+  let i = 0;
+  while (i < 10) {
+    let shooter = function() { // функция shooter
+      alert( i ); // должна выводить порядковый номер
+    };
+    shooters.push(shooter);
+    i++;
+  }
+
+  return shooters;
+}
+
+let army = makeArmy();
+
+army[0](); // у 0-го стрелка будет номер 10
+army[5](); // и у 5-го стрелка тоже будет номер 10
+// ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3...
+Почему у всех стрелков одинаковые номера? Почините код, чтобы он работал как задумано.
+*/
+
+function makeArmy() {
+  let shooters = [];
+
+  for (let i = 0; i < 10; i++) {
+    
+    let shooter = function() { // функция shooter
+      alert( i ); // должна выводить порядковый номер
+    };
+
+    shooters.push(shooter);    
+  };
+
+  return shooters;
+};
